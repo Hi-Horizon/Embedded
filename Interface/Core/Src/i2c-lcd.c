8874cbf100d94bbce051f1d01bfa85e149a2da6f
SenneDrent <sennedrent@gmail.com>
2026-06-04 09:52:41 +0200

    for the 3 reset commands, use a nibble sending scheme instead of lcd_send_cmd

--- Interface/Core/Src/i2c-lcd.c
+++ Interface/Core/Src/i2c-lcd.c
@@ -19,6 +19,16 @@ void lcd_send_cmd (char cmd)
 	HAL_I2C_Master_Transmit (&hi2c1, SLAVE_ADDRESS_LCD,(uint8_t *) data_t, 4, 100);
 }
 
+void lcd_send_nibble (char cmd)
+{
+  char data_u, data_l;
+	uint8_t data_t[2];
+	data_u = (cmd&0xf0);
+	data_t[0] = data_u|0x0C;  //en=1, rs=0
+	data_t[1] = data_u|0x08;  //en=0, rs=0
+	HAL_I2C_Master_Transmit (&hi2c1, SLAVE_ADDRESS_LCD,(uint8_t *) data_t, 2, 100);
+}
+
 void lcd_send_data (char data)
 {
 	char data_u, data_l;
@@ -45,13 +55,13 @@ void lcd_init (void)
 {
 	// 4 bit initialisation
 	HAL_Delay(50);  // wait for >40ms
-	lcd_send_cmd (0x30);
+	lcd_send_nibble (0x30);
 	HAL_Delay(5);  // wait for >4.1ms
-	lcd_send_cmd (0x30);
+	lcd_send_nibble (0x30);
 	HAL_Delay(1);  // wait for >100us
-	lcd_send_cmd (0x30);
+	lcd_send_nibble (0x30);
 	HAL_Delay(10);
-	lcd_send_cmd (0x20);  // 4bit mode
+	lcd_send_nibble (0x20);  // 4bit mode
 	HAL_Delay(10);
 
   // dislay initialisation
@@ -60,8 +70,7 @@ void lcd_init (void)
 	lcd_send_cmd (0x08); //Display on/off control --> D=0,C=0, B=0  ---> display off
 	HAL_Delay(1);
 	lcd_send_cmd (0x01);  // clear display
-	HAL_Delay(1);
-	HAL_Delay(1);
+	HAL_Delay(2);
 	lcd_send_cmd (0x06); //Entry mode set --> I/D = 1 (increment cursor) & S = 0 (no shift)
 	HAL_Delay(1);
 	lcd_send_cmd (0x0C); //Display on/off control --> D = 1, C and B = 0. (Cursor and blink, last two bits)
